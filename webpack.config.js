fbebc27d0c0f98b99e6a0124298ea5ddd96cf851
Jean-MarcJ <jmjbup@gmail.com>
2021-06-15 18:40:27 +0200

    Feat. Add files needed by webpack.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    watch: true,
+    entry: './assets/js/app.js',
+    output: {
+        path: path.resolve('./dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                } 
+            }
+        ]
+    }
+}
