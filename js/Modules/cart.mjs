99340dbdbee8344e7418c57354deb1e29e855f64
Jean-MarcJ <jmjbup@gmail.com>
2021-06-24 17:58:27 +0200

    Feat. Add cart file.

---
+++ js/Modules/cart.mjs
@@ -0,0 +1,13 @@
+// JavaScript Object for the cart management
+
+let cart = {
+    name = 'Panier',
+    items = [],
+    totalItemsInCart = 0,
+    subtotal = 0,
+    taxPercentage = 20,
+    Shipping = 15,
+    totalToBePaid = 0,
+
+
+}
