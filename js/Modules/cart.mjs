2a8dff35e35f4a554a5d042e86b75ac27db1878c
Jean-MarcJ <jmjbup@gmail.com>
2021-06-29 18:20:39 +0200

    Feat. Update cart functionality to sort items array.

--- js/Modules/cart.mjs
+++ js/Modules/cart.mjs
@@ -18,16 +18,15 @@ function cartInitialization () {
 
 cartInitialization()
 
-// ******************************
-// check if items are in the cart
-// ******************************
+// *****************************************
+// Check if there are items on local storage
+// *****************************************
 
 function fetchItemsInLocalStorage() {
     const local = JSON.parse(localStorage.getItem("items"))
 
     if (local != null) {
         
-        // console.log('Articles présents dans localStorage ! (from cart.mjs)', local)
         // Return an array of properties name inside the 'local' object (["0"], ["1"] ...)
         const properties = Object.keys(local);
         
@@ -37,7 +36,6 @@ function fetchItemsInLocalStorage() {
         updateBadgeIcon(totalItemInCart)
 
         // 2° Create & Add items from the localStorage to the cart items array
-
         for (const propertie of properties) {
             let item = local[propertie];
             // console.log('Dans local nom de l\'article : ', item.name);
@@ -67,9 +65,13 @@ function fetchItemsInLocalStorage() {
 
         }
 
-        // 3° Update cart isEmpty property
+        // 3° Update cart isEmpty property 
         localStorage.setItem("cartIsEmpty", false)
         cart.isEmpty = false
+
+        // 4° Sort & re-write cart array
+        sortCartArray(cart.items)
+
         
     } else {
         console.log('Aucun article dans localStorage cart.mjs !')
@@ -77,7 +79,6 @@ function fetchItemsInLocalStorage() {
         // 1° Update the badge icon with number 0
         updateBadgeIcon(0)
         
-        // 2° Afficher dans le template que le panier est vide.
     }
 }
 
@@ -96,6 +97,10 @@ async function addToCart(itemId, quantity) {
 
     // TODO:
     // Vérifier si l'id de l'article existe déjà dans le tableau
+    let itemsCartArray = cart.items
+    console.log('Array brut', itemsCartArray)
+    // itemsCartArray.sort()
+    // console.log('Array trié', itemsCartArray)
     
     // Create JSobject
     const itemToAdd = {
@@ -110,22 +115,21 @@ async function addToCart(itemId, quantity) {
     cart.items.push(itemToAdd)
     // console.log(cart.items.length, ' Article(s) dans votre panier')
     // console.log('Liste des articles dans votre panier :', cart.items)
+
+    // **** Save cart item in local Storage as object ****
+    localStorage.setItem("items", JSON.stringify(cart.items))
     
     //Compute the total amount to add
     const amountToAdd = computeTotalAmount(itemToAdd.price, itemToAdd.quantity)
     cart.subtotal += amountToAdd
     // console.log("S/Total du panier : ", cart.subtotal)
 
-    // **** Save cart item in local Storage as object ****
-    localStorage.setItem("items", JSON.stringify(cart.items))
-
     // **** Change the cart.isEmpty value ***
     cart.isEmpty = false
     localStorage.setItem("cartIsEmpty", false)
 }
-
-// addToCart("5be9c4471c9d440000a730e8", 1)
 // addToCart("5be1ed3f1c9d44000030b061", 2)
+// addToCart("5be9c4471c9d440000a730e8", 1)
 // addToCart("5be1ed3f1c9d44000030b061", 1)
 
 // ********************************
@@ -148,6 +152,77 @@ function computeTotalAmount(price, quantity) {
 function updateBadgeIcon(totalItemInCart) {
     // console.log('Update badge icon', itemInCart)
     document.getElementById("badge").textContent = totalItemInCart
+}
+
+// ********************************
+// Sort and update cart array
+// Should be a module ?
+// ********************************
+function sortCartArray(itemsArray) {
+    // console.log('Je veux trier le tableau : ', itemsArray)
+    
+    // Sort the given array by id
+    itemsArray.sort(function compare(a, b) {
+        if (a.id < b.id)
+            return -1
+        if (a.id > b.id)
+            return 1;
+        return 0;
+    })
+
+    console.log('sortCartArray : sorted by id ', itemsArray)
+    
+    // The first element of each element inside the array store the total quantity 
+    // The result is the same array with the total quantity in the first element
+    const properties = Object.keys(itemsArray);
+    // console.log('SortCartArray : properties :', properties) // 0:"0", 1:"1"
+    const totalItemInArray = Object.keys(itemsArray).length
+    // console.log('SortCartArray : totalItemInArray : ', totalItemInArray)
+    
+
+    for (const propertie of properties) {
+        let item = itemsArray[propertie];
+        let item1 = itemsArray[parseInt(propertie, 10) + 1];
+        let tempQuantity = parseInt(itemsArray[propertie].quantity);
+        // console.log('properties ', item1)
+
+        // console.log(item.name, item.quantity)
+        
+        if (parseInt(propertie, 10) + 1 < totalItemInArray && item.id === item1.id)  {
+            tempQuantity += item1.quantity;
+            item1.quantity = tempQuantity;
+
+        } else {
+            tempQuantity = parseInt(itemsArray[propertie].quantity);
+        }
+    }
+
+    //Return a new array without duplicated objects
+    // function removeDuplicates(originalArray, prop) {
+    //     var newArray = [];
+    //     var lookupObject = {};
+
+    //     for (var i in originalArray) {
+    //         lookupObject[originalArray[i][prop]] = originalArray[i];
+    //     }
+
+    //     for (i in lookupObject) {
+    //         newArray.push(lookupObject[i]);
+    //     }
+    //     return newArray;
+    // }
+
+    // let uniqueArray = removeDuplicates(itemsArray, "name");
+    const uniqueArray = [...new Map(itemsArray.map(item => [item["name"], item])).values()]
+    // console.log("uniqueArray is: " + JSON.stringify(uniqueArray));
+    console.log('uniqueArray parsed ', Array.from(uniqueArray));
+
+    // const itemsArrayFilter = itemsArray.filter(function(value) {
+    //     return value.indexOf("5be1ed3f1c9d44000030b061")
+    // });
+    
+    
+
 }
 
 // ********************************
