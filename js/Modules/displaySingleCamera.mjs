21bda74dbe256a0b0cc1b47fe578a1b7524a8b36
Jean-MarcJ <jmjbup@gmail.com>
2021-07-05 16:50:19 +0200

    Fix. Minors corrections.

--- js/Modules/displaySingleCamera.mjs
+++ js/Modules/displaySingleCamera.mjs
@@ -1,36 +1,33 @@
 function displaySingleCamera(camera) {
     
-    const templateElement = document.getElementById('single-item-template')
-    const cloneElement = document.importNode(templateElement.content, true)
+    const templateElement = document.getElementById('single-item-template');
+    const cloneElement = document.importNode(templateElement.content, true);
 
     // Display camera's lenses otpions
-    const lensesOption = camera.lenses
-    const lensesOptionLength = lensesOption.length
-    for (let i = 0; i < lensesOptionLength; i++) {
+    const lensesOption = camera.lenses;
+    for (let i = 0; i < lensesOption.length; i++) {
         cloneElement.getElementById("lense-choice")
             .innerHTML += '<option value="' + lensesOption[i] + '">' + lensesOption[i] + '</option>'
     }
 
     // Product Name
-    cloneElement.getElementById("item-name").textContent = camera.name
+    cloneElement.getElementById("item-name").textContent = camera.name;
     
     // Product img src url
-    cloneElement.getElementById("item-img_src").setAttribute("src", camera.imageUrl)
+    cloneElement.getElementById("item-img_src").setAttribute("src", camera.imageUrl);
     
     // Product price
     const price = camera.price;
-    cloneElement.getElementById("item-price").textContent = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(price)
+    cloneElement.getElementById("item-price").textContent = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(price);
     
     // Product descriptions
-    cloneElement.getElementById("item-description").textContent = camera.description
+    cloneElement.getElementById("item-description").textContent = camera.description;
     
     // Product id
-    cloneElement.getElementById("item-id").textContent = camera._id
+    cloneElement.getElementById("item-id").textContent = camera._id;
 
     // Child clone injection in DOM
-    document.getElementById("single-item").appendChild(cloneElement)
-
-    //TODO: personnaliser la balise title avec le nom du produit.
+    document.getElementById("single-item").appendChild(cloneElement);
 }
 
 export { displaySingleCamera }
