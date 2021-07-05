e2069e1abf61709d23f4996bfdee8d39e7ecaee5
Jean-MarcJ <jmjbup@gmail.com>
2021-07-05 09:38:15 +0200

    Fix. ready to use display errors functionnality.

--- js/Modules/fetchCameras.mjs
+++ js/Modules/fetchCameras.mjs
@@ -1,22 +1,18 @@
 const url = "http://localhost:3000/api/cameras/";
-// const url = "https://jsonplaceholder.typicode.com/users"
 
-// Fetch all cameras
-// function statement
+// Fetch all cameras - function statement
+
 async function getCameras() {
     try {
 
-        // const response = await fetch(url)
         return (await fetch(url)).json();
-
-        //Old code
-        // Retourne une promise. Transforme le contenu JSON en objet
-        // const data = await response.json()
-        // return data
 
     } catch(error) {
         // console.log('Erreur dans le chargement des produits')
         alert('Erreur dans le chargement des produits... \n' + error)
+        // const message = 'Erreur dans le chargement des produits...';
+        // const errorClass = 'danger';
+        // displayError(message, error, errorClass);
     }
 }
 
