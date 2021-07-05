fe2eb8f47ad44508f36e02d509a3dfd02115fd28
Jean-MarcJ <jmjbup@gmail.com>
2021-07-05 16:52:25 +0200

    Fix. Code factoring.

--- js/main.js
+++ js/main.js
@@ -1,74 +1,70 @@
-import { getCameras } from './Modules/fetchCameras.mjs'
-import { displayCamera } from './Modules/displayCamera.mjs'
-import { cart } from './Modules/cart.mjs'
+import { getCameras } from './Modules/fetchCameras.mjs';
+import { displayCamera } from './Modules/displayCamera.mjs';
+import { cart, addToCart } from './Modules/cart.mjs';
 
 
-main()
+main();
 
 // Fonctionnalité principale de la page
 
 async function main() {
     
     try {
-        const cameras = await getCameras()
-        console.log('Liste des APN :', cameras)
+        const cameras = await getCameras();
+        console.log('Liste des APN :', cameras);
         
-        const itemsInCollection = cameras.length
-        // console.log(typeof(cameras))
         // Display all cameras
-        for (let i = 0; i < itemsInCollection; i++) {
-            const camera = cameras[i]
-            displayCamera(camera)
+        for (let i = 0; i < cameras.length; i++) {
+            const camera = cameras[i];
+            displayCamera(camera);
+        }
+
+        const targetAddToCart = document.querySelectorAll(".btn-addtocart");
+        
+        for (const targetAddToCartElem of targetAddToCart) {
+            targetAddToCartElem.addEventListener("click", addToCartAction);
         }
 
     } catch(error) {
         // alert("Erreur de connection avec le serveur ! \n" + error)
-        const message = "Une erreur est intervenue !"
-        const errorClass = "danger"
-        displayError(message, error, errorClass)
+        const message = "Une erreur est intervenue !";
+        const errorClass = "danger";
+        displayError(message, error, errorClass);
     }
 
     //Cart management
 
-    if (localStorage.getItem("cartIsEmpty") == "true") {
-        console.log("Votre panier est vide")
-        
-        // badge icon should print 0
-        // document.getElementById("badge").textContent = "0"
-        // La fenêtre du panier doit être vide
-
-        // Le total du panier = 0
+    if (localStorage.getItem("cartIsEmpty") === "true") {
+        console.log("Votre panier est vide");
        
-
     } else {
-        
-        console.log("Vous avez des articles dans votre panier !")
-        console.log("Etat de l'objet panier : ", cart)
-        // console.log("Nb Article dans l'objet cart ", cart.items.length)
-        
+        localStorage.setItem("cartIsEmpty", false);
+        // console.log("Vous avez des articles dans votre panier !");
+        console.log("Etat de l'objet panier : ", cart);
     }
 }
 
 function displayError(message, error, errorClass) {
-    console.log(message, error, errorClass)
+    // console.log(message, error, errorClass);
     
     errorClass = "alert-" + errorClass
     
     let elt = document.getElementById("error")
-        .classList.add(errorClass)
+        .classList.add(errorClass);
     
     let errorMessageClass = document.getElementById("error-message")
-        .classList.add("p-5")
+        .classList.add("p-5");
 
     let errorMessage = document.getElementById("error-message")
-        .textContent = message + error
+        .textContent = message + error;
 
 }
 
 // ********************************
-// Clear cart
+// Call the function addToCart on
 // ********************************
-clearCart.onclick = () => {
-    localStorage.clear()
-    document.location.reload()
+
+function addToCartAction() {
+    let itemId = this.getAttribute("data-item-id");
+    addToCart(itemId, 1);
 }
