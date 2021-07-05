b96b0756b2bb7255b956e5b77000a668caf0e206
Jean-MarcJ <jmjbup@gmail.com>
2021-07-05 16:32:58 +0200

    Fix. EId erased. Class instead for the template injection.

--- js/Modules/displayCamera.mjs
+++ js/Modules/displayCamera.mjs
@@ -1,24 +1,45 @@
 function displayCamera(camera) {
 
-    const templateElement = document.getElementById('itemTemplate')
-    const cloneElement = document.importNode(templateElement.content, true)
+    const templateElement = document.getElementById('itemTemplate');
+    const cloneElement = document.importNode(templateElement.content, true);
 
-    // URL re-writing to get one product
-    const singleItemlink = "http://127.0.0.1:5500/templates/shop-single.html?id=" + camera._id
-
-    cloneElement.getElementById("item_single-link")
-        .setAttribute("href", singleItemlink )
-
-    // Product Name
-    cloneElement.getElementById("item-name").textContent = camera.name
-    // Product Name single link
-    cloneElement.getElementById("item-name").setAttribute("href", singleItemlink)
-    // Prudct img src url
-    cloneElement.getElementById("item-img_src").setAttribute("src", camera.imageUrl)
-    // Product price
-    const price = camera.price;
-    cloneElement.getElementById("item-price").textContent = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(price)
+    // URL to get one product (with id)
+    const singleItemlink = "http://127.0.0.1:5500/templates/shop-single.html?id=" + camera._id;
     
+    // Image single link
+    const imgLink = cloneElement.querySelectorAll('.card-single-link');
+    for (const item of imgLink) {
+        item.setAttribute("href", singleItemlink);
+    }
+
+    // Product img src url
+    const imgSrc = cloneElement.querySelectorAll('.item-img_src');
+    for (const item of imgSrc) {
+        item.setAttribute("src", camera.imageUrl);
+    }
+
+    // Product Name & single link
+    const itemsName = cloneElement.querySelectorAll('.item-name');
+    for (const item of itemsName) {
+        item.textContent = camera.name;
+        item.setAttribute("href", singleItemlink);
+    }
+    
+    // Product price
+    const itemsPrice = cloneElement.querySelectorAll('.item-price');
+    for (const item of itemsPrice) {
+        item.textContent = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(camera.price);
+    }
+    
+    // data-item-index -> item id
+    // cloneElement.getElementById("addToCart").setAttribute("data-item-id", camera._id);
+    const itemsId = cloneElement.querySelectorAll('.btn-addtocart');
+    for (const item of itemsId) {
+        item.setAttribute("data-item-id", camera._id);
+    }
+
+
+
     // Child clone injection in DOM
     document.getElementById("shop-grid").appendChild(cloneElement)
 
