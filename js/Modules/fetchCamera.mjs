be45bde110af6bc185e261557871c31ebd6c9fff
jeanmarcj <jmjbup@gmail.com>
2021-07-19 14:29:41 +0200

    Fix. Code indentation.

--- js/Modules/fetchCamera.mjs
+++ js/Modules/fetchCamera.mjs
@@ -1,15 +1,14 @@
 
 // Get the item with th id given in the URI
+
 let parsedUrl = new URL(window.location.href);
 const id = parsedUrl.searchParams.get("id");
-
-
 const url = "http://localhost:3000/api/cameras/" + id;
 
 /**
  * Get one camera with the id given in the url.
  * 
- * @returns {object} the camera fetched by id in the url.
+ * @returns {object} the camera fetched by id.
  */
 async function getCamera() {
 
