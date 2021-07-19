9bdd9df12c5de3d977df34f13fb83f136de2368b
jeanmarcj <jmjbup@gmail.com>
2021-07-19 14:34:41 +0200

    Fix. Code refectoring and indentation.

--- js/Modules/fetchCameras.mjs
+++ js/Modules/fetchCameras.mjs
@@ -8,16 +8,11 @@ const url = "http://localhost:3000/api/cameras/";
  * @returns {object} The cameras fetched object.
  */
 async function getCameras() {
+    
     try {
-
         return (await fetch(url)).json();
-
     } catch(error) {
-        // console.log('Erreur dans le chargement des produits')
         alert('Erreur dans le chargement des produits... \n' + error)
-        // const message = 'Erreur dans le chargement des produits...';
-        // const errorClass = 'danger';
-        // displayError(message, error, errorClass);
     }
 }
 
