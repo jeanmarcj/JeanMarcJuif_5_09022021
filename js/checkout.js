d77834ba1690576cdcff4db0d4f32992d98fac44
jeanmarcj <jmjbup@gmail.com>
2021-07-23 13:30:10 +0200

    Fix. Comments updated & else condition in checkForm() erased.

--- js/checkout.js
+++ js/checkout.js
@@ -89,17 +89,17 @@ function checkForm() {
     return false;
   } else if (!testRegex(email, "email", /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/)) {
     return false;
-  } else {
-    purchaseOrder.contact.lastName = lastName;
-    purchaseOrder.contact.firstName = firstName;
-    purchaseOrder.contact.address = address;
-    purchaseOrder.contact.city = city;
-    purchaseOrder.contact.email = email; 
-    
-    return true;
   }
-  
+
+  purchaseOrder.contact.lastName = lastName;
+  purchaseOrder.contact.firstName = firstName;
+  purchaseOrder.contact.address = address;
+  purchaseOrder.contact.city = city;
+  purchaseOrder.contact.email = email;
+    
+  return true;
 }
+
 /**
  * Test a regex rule for an input.
  * Apply the setCustomValidity function to the element id.
@@ -153,8 +153,6 @@ function submitOrder(purchaseOrder) {
     }
   })
   .then(function(datas){
-    console.log(datas);
-    // alert('Je suis dans datas !');
     localStorage.setItem("orderCustomer", JSON.stringify(datas.contact));
     localStorage.setItem("orderId", JSON.stringify(datas.orderId));
     localStorage.setItem("orderProducts", JSON.stringify(datas.products));
